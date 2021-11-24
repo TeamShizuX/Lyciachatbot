6023c284460f70fbd6b93ca5e7f69bb4048ddd61
TeamShizu <88240053+TeamShizuX@users.noreply.github.com>
2021-11-24 13:45:52 +0530

    Update config.py

--- lycia/config.py
+++ lycia/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("5948661")
+    API_HASH = "ed081430c75f3e0b9a95b3b5d461fa73"
+    TOKEN = "2125465031:AAEBSl6WiRO3ixIWUeHRa1GUWvac0HQFx3k"
